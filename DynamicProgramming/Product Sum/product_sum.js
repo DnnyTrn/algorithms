22302721851a4dde7e7060f088e7ffeeee6d152a
Danny <trandan@oregonstate.edu>
2018-10-22 09:55:44 -0700

    Update product_sum.js

--- DynamicProgramming/Product Sum/product_sum.js
+++ DynamicProgramming/Product Sum/product_sum.js
@@ -9,11 +9,9 @@ let product_sum = (OPT, n) => {
     }
 
     //if(n >= 2)
-    return Math.max(product_sum(OPT, n-1)+OPT[n-1],
-    product_sum(OPT, n-2) + OPT[n-1]* OPT[n-2]);
+    return Math.max((product_sum(OPT, n-1)+ OPT[n-1]), (product_sum(OPT, n-2) + OPT[n-1] * OPT[n-2]));
 
 };
-
 
 var list = [2, 2, 1, 3, 2, 1, 2, 2, 1, 2];
 console.log(product_sum(list,list.length))
