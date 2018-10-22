4dd004fc3d9e6d8aee8339ba32dc74fb4fc0ea8f
Danny <trandan@oregonstate.edu>
2018-10-22 09:53:46 -0700

    Create product_sum.js

---
+++ DynamicProgramming/Product Sum/product_sum.js
@@ -0,0 +1,19 @@
+let product_sum = (OPT, n) => {
+    
+    if(n === 0){
+        return 0;
+    }
+
+    if(n === 1) {
+        return OPT[0];
+    }
+
+    //if(n >= 2)
+    return Math.max(product_sum(OPT, n-1)+OPT[n-1],
+    product_sum(OPT, n-2) + OPT[n-1]* OPT[n-2]);
+
+};
+
+
+var list = [2, 2, 1, 3, 2, 1, 2, 2, 1, 2];
+console.log(product_sum(list,list.length))
