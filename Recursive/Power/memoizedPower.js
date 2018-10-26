41e16ef6fc164fea40801b22b5088939469113b8
Danny <trandan@oregonstate.edu>
2018-10-26 10:01:44 -0700

    Update memoizedPower.js

--- Recursive/Power/memoizedPower.js
+++ Recursive/Power/memoizedPower.js
@@ -1,1 +1,25 @@
 
+var power = (x,n) => {
+
+
+    if(n === 0){
+        return 1;
+    }
+
+    if(n === 1){
+        return x;
+    }
+
+    //if n is even
+    if(!(n % 2))
+    {
+        temp = power(x, n/2);
+        return temp * temp;
+    }
+    else    //n is odd
+    {
+        temp = power(x, (n-1)/2);
+        return x * temp * temp;
+    }
+
+}
