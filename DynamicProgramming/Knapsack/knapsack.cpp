8066f902c432f68ad85208d5a5be7cb6ea58295a
Danny <trandan@oregonstate.edu>
2018-10-25 18:04:53 -0700

    Update knapsack.cpp

--- DynamicProgramming/Knapsack/knapsack.cpp
+++ DynamicProgramming/Knapsack/knapsack.cpp
@@ -1,8 +1,18 @@
 /*psuedocode
 
-if(P[i].days < Days) {
-    OPT(i,j) = max( OPT(i-1,j)  ,   OPT(i-j, j-P[i].days) + P[i].fee)
+
+for(i = 1 to number_of_jobs) {
+    for(j = 0, j to number_of_days) {
+        //enough time to complete project
+        if(P[i].days < Days) {
+            OPT(i,j) = max( OPT(i-1,j)  ,   OPT(i-j, j-P[i].days) + P[i].fee);
+        }
+        else    //not enough time to complete project
+        {
+            OPT(i,j) = OPT(i - 1, j);
+        }
+    }
 }
-
-
 */
+
+
